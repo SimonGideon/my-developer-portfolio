b134d0f231de490b0ee7ed7519d70c5c596111e7
SimonGideon <simongideon918@gmail.com>
2023-07-10 23:33:34 +0300

    Handle form submissions

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,6 @@
 import { useState, useRef } from 'react';
 import { motion } from 'framer-motion';
+import swal from 'sweetalert';
 import emailjs from '@emailjs/browser';
 import { styles } from '../styles';
 import { SectionWrapper } from '../hoc';
@@ -20,30 +21,31 @@ const Contact = () => {
 
     setForm({ ...form, [name]: value });
   };
-
+  
   const handleSubmit = (e) => {
     e.preventDefault();
     setLoading(true);
-
-    // sign up on emailjs.com (select the gmail service and connect your account).
-    //click on create a new template then click on save.
     emailjs
       .send(
-        'serviceID', // paste your ServiceID here (you'll get one when your service is created).
-        'templateID', // paste your TemplateID here (you'll find it under email templates).
+        'service_tmz3lye',
+        'template_gh1wsu5',
         {
           from_name: form.name,
-          to_name: 'YourName', // put your name here.
+          to_name: 'Simon Gideon',
           from_email: form.email,
-          to_email: 'youremail@gmail.com', //put your email here.
+          to_email: 'simongideon918@gmail.com',
           message: form.message,
         },
-        'yourpublickey' //paste your Public Key here. You'll get it in your profile section.
+        'RRhuQysRw3tCC4ITi',
       )
       .then(
         () => {
           setLoading(false);
-          alert('Thank you. I will get back to you as soon as possible.');
+          swal({
+            title: "Message Sent!",
+            text: "I'll get back to you as soon as possible.",
+            icon: "success",
+          });
 
           setForm({
             name: '',
