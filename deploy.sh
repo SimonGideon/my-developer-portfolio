0666f90adcef77e3aeb5cf5b3694a5887cbb391d
SimonGideon <simongideon918@gmail.com>
2023-07-11 22:18:45 +0300

    Initial commit

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ git commit -m "deploy"
 #git push -f git@github.com:SimonGideon/my-developer-portfolio.github.io.git main
 
 #if you are deploying to https://<USERNAME>.github.io/<REPO>
-#git push -f git@github.com:SimonGideon/my-developer-portfolio.git main:gh-pages
+#git push -f git@github.com:<USERNAME>/<REPONAME>.git main:gh-pages
 
 cd -
