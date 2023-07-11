0fed820bcc43ba6fc081df6b331e21713e8cb287
SimonGideon <simongideon918@gmail.com>
2023-07-11 21:58:20 +0300

    Deploy

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#place .nojekyll to bypass jekyll processing
+echo >.nojekyll
+#if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+git init
+git checkout main
+git add .
+git commit -m "deploy"
+
+#if you are deploying to https://<USERNAME>.github.io
+#git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+#if you are deploying to https://<USERNAME>.github.io/<REPO>
+#git push -f git@github.com:SimonGideon/my-developer-portfolio.git main:gh-pages
+
+cd -
