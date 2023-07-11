0444cf7ac2f351d0d7c4a5cb1d9f71d02f378a87
SimonGideon <simongideon918@gmail.com>
2023-07-12 02:13:15 +0300

    Links open on different window

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -2,30 +2,42 @@ import { instagram, linkedin, twitter } from "../assets"
 const socials = () => {
     return (
         <span className=" animate-move relative background">
-            <span className="social-media-buttons">
+            <span id="socials-ico" className="social-media-buttons">
                 <span className="social-media-button">
-                    <a href="https://www.instagram.com/simon_gideon_/">
-                        <img
-                            alt="Instagram"
-                            src={instagram}
-                        />
-                    </a>
+                    <img
+                        alt="Instagram"
+                        src={instagram}
+                        onClick={() =>
+                            window.open(
+                                'https://www.instagram.com/simon_gideon_/',
+                                '_blank'
+                            )
+                        }
+                    />
                 </span>
                 <span className="social-media-button">
-                    <a href="https://twitter.com/Simo_Giddy">
-                        <img
-                            alt="Twitter"
-                            src={twitter}
-                        />
-                    </a>
+                    <img
+                        alt="Twitter"
+                        src={twitter}
+                        onClick={() =>
+                            window.open(
+                                "https://twitter.com/Simo_Giddy",
+                                '_blank'
+                            )
+                        }
+                    />
                 </span>
                 <span className="social-media-button">
-                    <a href="https://www.linkedin.com/in/simon-gideon/">
-                        <img
-                            alt="LinkedIn"
-                            src={linkedin}
-                        />
-                    </a>
+                    <img
+                        alt="LinkedIn"
+                        src={linkedin}
+                        onClick={() =>
+                            window.open(
+                                "https://www.linkedin.com/in/simon-gideon/",
+                                '_blank'
+                            )
+                        }
+                    />
                 </span>
             </span>
         </span>
