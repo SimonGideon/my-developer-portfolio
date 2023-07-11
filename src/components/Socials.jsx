dbf54a91fb8e0de2887775df6d953660fe4addfe
SimonGideon <simongideon918@gmail.com>
2023-07-11 03:15:03 +0300

    Socials styling

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,28 +1,34 @@
 import { instagram, linkedin, twitter } from "../assets"
-const socials = ()=>{
+const socials = () => {
     return (
-        <span className="background">
-        <span className="social-media-buttons">
-            <span className="social-media-button">
-                <img
-                    alt="Instagram"
-                    src={instagram}
-                />
-            </span>
-            <span className="social-media-button">
-                <img
-                    alt="Twitter"
-                    src={twitter}
-                />
-            </span>
-            <span className="social-media-button">
-                <img
-                    alt="LinkedIn"
-                    src={linkedin}
-                />
+        <span className="relative background">
+            <span className="social-media-buttons">
+                <span className="social-media-button">
+                    <a href="https://www.instagram.com/simon_gideon_/">
+                        <img
+                            alt="Instagram"
+                            src={instagram}
+                        />
+                    </a>
+                </span>
+                <span className="social-media-button">
+                    <a href="https://twitter.com/Simo_Giddy">
+                        <img
+                            alt="Twitter"
+                            src={twitter}
+                        />
+                    </a>
+                </span>
+                <span className="social-media-button">
+                    <a href="https://www.linkedin.com/in/simon-gideon/">
+                        <img
+                            alt="LinkedIn"
+                            src={linkedin}
+                        />
+                    </a>
+                </span>
             </span>
         </span>
-    </span>
     )
 }
 
