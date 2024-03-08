81a56a65696ab5e4302d240fad9ee74128d3ca7b
SimonGideon <simongideon918@gmail.com>
2024-03-08 10:06:43 +0300

    Initial commit

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-#place .nojekyll to bypass jekyll processing
-echo >.nojekyll
-#if you are deploying to a custom domain
-# echo 'www.simongideon.tech' > CNAME
-git init
-git checkout main
-git add .
-git commit -m "deploy"
+# #place .nojekyll to bypass jekyll processing
+# echo >.nojekyll
+# #if you are deploying to a custom domain
+# # echo 'www.simongideon.tech' > CNAME
+# git init
+# git checkout main
+# git add .
+# git commit -m "deploy"
 
-#if you are deploying to https://<USERNAME>.github.io
-#git push -f git@github.com:SimonGideon/my-developer-portfolio.github.io.git main
+# #if you are deploying to https://<USERNAME>.github.io
+# #git push -f git@github.com:SimonGideon/my-developer-portfolio.github.io.git main
 
-#if you are deploying to https://<USERNAME>.github.io/<REPO>
-#git push -f git@github.com:<USERNAME>/<REPONAME>.git main:gh-pages
+# #if you are deploying to https://<USERNAME>.github.io/<REPO>
+# #git push -f git@github.com:<USERNAME>/<REPONAME>.git main:gh-pages
 
-cd -
+# cd -
