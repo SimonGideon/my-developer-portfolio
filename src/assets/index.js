265f32d8a28facf0e74387a38f20525a911ffefb
SimonGideon <simongideon918@gmail.com>
2024-05-06 16:02:53 +0300

    Add resume pdf

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,64 +1,67 @@
-import bwmap from './backgrounds/bw-map.jpeg';
-import network from './backgrounds/network.png';
-import nairobi from './backgrounds/nairobi.png';
-import whiteabstract from './backgrounds/white-abstract.png';
-import worldmap from './backgrounds/world-map.png';
+import bwmap from "./backgrounds/bw-map.jpeg";
+import network from "./backgrounds/network.png";
+import nairobi from "./backgrounds/nairobi.png";
+import whiteabstract from "./backgrounds/white-abstract.png";
+import worldmap from "./backgrounds/world-map.png";
 
-import logo from './logo/logo-black.png';
-import logotext from './logo/logo-text-black.png';
+import logo from "./logo/logo-black.png";
+import logotext from "./logo/logo-text-black.png";
 
-import simon from './personal/simon.png';
-import linux from './tech/linux.png'
+import simon from "./personal/simon.png";
+import linux from "./tech/linux.png";
 
-import backend from './icons/backend.png';
-import ux from './icons/ux.png';
-import frontend from './icons/frontend.png';
-import prototyping from './icons/prototyping.png';
-import github from './icons/github.png';
-import close from './icons/close.png';
-import download from './icons/download.png';
-import downloadHover from './icons/downloadHover.png';
-import pineapple from './icons/pineapple.png';
-import pineappleHover from './icons/pineappleHover.png';
-import menu from './icons/menu.png';
-import resume from './icons/resume.png';
-import send from './icons/send.png';
-import sendHover from './icons/sendHover.png';
+import backend from "./icons/backend.png";
+import ux from "./icons/ux.png";
+import frontend from "./icons/frontend.png";
+import prototyping from "./icons/prototyping.png";
+import github from "./icons/github.png";
+import close from "./icons/close.png";
+import download from "./icons/download.png";
+import downloadHover from "./icons/downloadHover.png";
+import pineapple from "./icons/pineapple.png";
+import pineappleHover from "./icons/pineappleHover.png";
+import menu from "./icons/menu.png";
+import resume from "./icons/resume.png";
+import send from "./icons/send.png";
+import sendHover from "./icons/sendHover.png";
 
-import css from './tech/css.png';
-import docker from './tech/docker.png';
-import figma from './tech/figma.png';
-import git from './tech/git.png';
-import html from './tech/html.png';
-import javascript from './tech/javascript.png';
-import nodejs from './tech/nodejs.png';
-import reactjs from './tech/reactjs.png';
-import redux from './tech/redux.png';
-import tailwind from './tech/tailwind.png';
-import typescript from './tech/typescript.png';
-import graphql from './tech/graphql.png';
-import postgresql from './tech/postgresql.png';
-import rubyrails from './tech/rubyrails.png';
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import graphql from "./tech/graphql.png";
+import postgresql from "./tech/postgresql.png";
+import rubyrails from "./tech/rubyrails.png";
 
-import coverhunt from './company/coverhunt.png';
-import dcc from './company/dcc.png';
-import kelhel from './company/kelhel.png';
-import microverse from './company/microverse.png';
+import coverhunt from "./company/coverhunt.png";
+import dcc from "./company/dcc.png";
+import kelhel from "./company/kelhel.png";
+import microverse from "./company/microverse.png";
 
 // projects images
-import globalsummit from './projects/global-sumit.png';
-import reservation from './projects/reservation.jpeg';
-import bookstore from './projects/bookstore.jpeg';
-import metric from './projects/metric-app.png';
-import spaceTravellers from './projects/space-travellers.jpeg';
-import space from './projects/space-hub.png';
-import lyriks from './projects/shazam-lyriks.png';
+import globalsummit from "./projects/global-sumit.png";
+import reservation from "./projects/reservation.jpeg";
+import bookstore from "./projects/bookstore.jpeg";
+import metric from "./projects/metric-app.png";
+import spaceTravellers from "./projects/space-travellers.jpeg";
+import space from "./projects/space-hub.png";
+import lyriks from "./projects/shazam-lyriks.png";
 
 // import socail icons
-import instagram from './socials/instagram.svg';
-import linkedin from './socials/linkedin.svg';
-import twitter from './socials/twitter.svg';
-import github_socials from './socials/github1.png';
+import instagram from "./socials/instagram.svg";
+import linkedin from "./socials/linkedin.svg";
+import twitter from "./socials/twitter.svg";
+import github_socials from "./socials/github1.png";
+
+// import resume
+import myResume from "../assets/personal/Simon_Gideon_Resume.pdf";
 
 export {
   lyriks,
@@ -81,6 +84,7 @@ export {
   pineappleHover,
   menu,
   resume,
+  myResume,
   send,
   sendHover,
   css,
--- src/constants/index.js
+++ src/constants/index.js
@@ -24,267 +24,245 @@ import {
   twitter,
   github_socials,
   lyriks,
-} from '../assets';
+} from "../assets";
 
 export const navLinks = [
   {
-    id: 'about',
-    title: 'About',
+    id: "about",
+    title: "About",
   },
   {
-    id: 'projects',
-    title: 'Projects',
+    id: "projects",
+    title: "Projects",
   },
   {
-    id: 'contact',
-    title: 'Contact',
+    id: "contact",
+    title: "Contact",
   },
 ];
 
 const services = [
   {
-    title: 'Frontend Developer',
+    title: "Frontend Developer",
     icon: frontend,
   },
   {
-    title: 'Backend Developer',
+    title: "Backend Developer",
     icon: backend,
   },
   {
-    title: 'UI/UX Design',
+    title: "UI/UX Design",
     icon: ux,
   },
   {
-    title: 'Software Prototyping',
+    title: "Software Prototyping",
     icon: prototyping,
   },
 ];
 
 const socialsM = [
   {
-    name:"Instagram",
+    name: "Instagram",
     icon: instagram,
-    link: 'https://www.instagram.com/simon_gideon_/'
+    link: "https://www.instagram.com/simon_gideon_/",
   },
   {
-    name:"Twitter",
+    name: "Twitter",
     icon: twitter,
-    link: "https://twitter.com/Simo_Giddy"
+    link: "https://twitter.com/Simo_Giddy",
   },
   {
-    name:"LinkedIn",
+    name: "LinkedIn",
     icon: linkedin,
-    link: "https://www.linkedin.com/in/simon-gideon/"
+    link: "https://www.linkedin.com/in/simon-gideon/",
   },
 
   {
     name: "GitHub",
     icon: github_socials,
-    link: "https://github.com/SimonGideon"
-  }
+    link: "https://github.com/SimonGideon",
+  },
 ];
 
 const technologies = [
   {
-    name: 'HTML 5',
+    name: "HTML 5",
     icon: html,
   },
   {
-    name: 'CSS 3',
+    name: "CSS 3",
     icon: css,
   },
   {
-    name: 'Linux',
+    name: "Linux",
     icon: linux,
   },
   {
-    name: 'JavaScript',
+    name: "JavaScript",
     icon: javascript,
   },
 
   {
-    name: 'React JS',
+    name: "React JS",
     icon: reactjs,
   },
   {
-    name: 'Redux Toolkit',
+    name: "Redux Toolkit",
     icon: redux,
   },
   {
-    name: 'Tailwind CSS',
+    name: "Tailwind CSS",
     icon: tailwind,
   },
   {
-    name: 'Rails',
+    name: "Rails",
     icon: rubyrails,
   },
 
   {
-    name: 'postgresql',
+    name: "postgresql",
     icon: postgresql,
   },
 
   {
-    name: 'git',
+    name: "git",
     icon: git,
   },
   {
-    name: 'figma',
+    name: "figma",
     icon: figma,
   },
 ];
 
 const experiences = [
   {
-    title: 'Mentor (Volunteer)',
-    company_name: 'Microverse',
+    title: "Mentor (Volunteer)",
+    company_name: "Microverse",
     icon: microverse,
-    iconBg: '#333333',
-    date: 'Aug 2022 - Aug 2023',
-  }
+    iconBg: "#333333",
+    date: "Aug 2022 - Aug 2023",
+  },
 ];
 
 const projects = [
   {
-    id: 'project-1',
-    name: 'GLOBAL UN REFUGEE SUMMIT',
-    description: 'UN Global Summit Advertisement Page that gives information to attendees about the Summit.',
+    id: "project-1",
+    name: "GLOBAL UN REFUGEE SUMMIT",
+    description:
+      "UN Global Summit Advertisement Page that gives information to attendees about the Summit.",
     tags: [
       {
-        name: 'react',
-        color: 'blue-text-gradient',
+        name: "react",
+        color: "blue-text-gradient",
       },
       {
-        name: 'mongodb',
-        color: 'green-text-gradient',
+        name: "mongodb",
+        color: "green-text-gradient",
       },
       {
-        name: 'tailwind',
-        color: 'pink-text-gradient',
+        name: "tailwind",
+        color: "pink-text-gradient",
       },
     ],
     image: globalsummit,
-    repo: ' https://github.com/SimonGideon/UN-Refugee-global-Summit',
-    demo: 'https://simongideon.github.io/capstone-project---Conference-page/',
-    stack: [
-      html,
-      css,
-      javascript
-    ]
+    repo: " https://github.com/SimonGideon/UN-Refugee-global-Summit",
+    demo: "https://simongideon.github.io/capstone-project---Conference-page/",
+    stack: [html, css, javascript],
   },
   {
-    id: 'project-2',
-    name: 'Alpha Reservation',
+    id: "project-2",
+    name: "Alpha Reservation",
     description:
-      'A reservation app that allows users to reserve a house and pay per night.',
+      "A reservation app that allows users to reserve a house and pay per night.",
     tags: [
       {
-        name: 'react',
-        color: 'blue-text-gradient',
+        name: "react",
+        color: "blue-text-gradient",
       },
       {
-        name: 'restapi',
-        color: 'green-text-gradient',
+        name: "restapi",
+        color: "green-text-gradient",
       },
       {
-        name: 'scss',
-        color: 'pink-text-gradient',
+        name: "scss",
+        color: "pink-text-gradient",
       },
     ],
     image: reservation,
-    repo: 'https://github.com/SimonGideon/Alpha-Reservation',
-    demo: 'https://alpha-reservation.vercel.app/',
-    stack: [
-      reactjs,
-      redux,
-      postgresql,
-      tailwind,
-      rubyrails,
-    ]
+    repo: "https://github.com/SimonGideon/Alpha-Reservation",
+    demo: "https://alpha-reservation.vercel.app/",
+    stack: [reactjs, redux, postgresql, tailwind, rubyrails],
   },
   {
-    id: 'project-3',
-    name: 'Bookstore',
-    description: 'React App that enables the user to add and see list of books from external API.',
+    id: "project-3",
+    name: "Bookstore",
+    description:
+      "React App that enables the user to add and see list of books from external API.",
     tags: [
       {
-        name: 'nextjs',
-        color: 'blue-text-gradient',
+        name: "nextjs",
+        color: "blue-text-gradient",
       },
       {
-        name: 'supabase',
-        color: 'green-text-gradient',
+        name: "supabase",
+        color: "green-text-gradient",
       },
       {
-        name: 'css',
-        color: 'pink-text-gradient',
+        name: "css",
+        color: "pink-text-gradient",
       },
     ],
     image: bookstore,
-    repo: 'https://github.com/SimonGideon/BookstoreApp',
-    demo: 'https://book-stores.onrender.com/',
-    stack: [
-      reactjs,
-      redux,
-      css
-    ]
+    repo: "https://github.com/SimonGideon/BookstoreApp",
+    demo: "https://book-stores.onrender.com/",
+    stack: [reactjs, redux, css],
   },
   {
-    id: 'project-4',
-    name: 'Metric App',
+    id: "project-4",
+    name: "Metric App",
     description: `A single-page application that shows the statistics if the Population in african countries.`,
     tags: [
       {
-        name: 'nextjs',
-        color: 'blue-text-gradient',
+        name: "nextjs",
+        color: "blue-text-gradient",
       },
       {
-        name: 'supabase',
-        color: 'green-text-gradient',
+        name: "supabase",
+        color: "green-text-gradient",
       },
       {
-        name: 'css',
-        color: 'pink-text-gradient',
+        name: "css",
+        color: "pink-text-gradient",
       },
     ],
     image: metric,
-    repo: 'https://github.com/SimonGideon/-React-capstone-project',
-    demo: 'https://metric-app.onrender.com/',
-    stack: [
-      reactjs,
-      redux,
-      css,
-      tailwind
-    ]
+    repo: "https://github.com/SimonGideon/-React-capstone-project",
+    demo: "https://metric-app.onrender.com/",
+    stack: [reactjs, redux, css, tailwind],
   },
   {
-    id: 'project-5',
-    name: 'My Lyriks App',
+    id: "project-5",
+    name: "My Lyriks App",
     description:
-      'Online Music App that play and display music county wide and world wide',
+      "Online Music App that play and display music county wide and world wide",
     tags: [
       {
-        name: 'react',
-        color: 'blue-text-gradient',
+        name: "react",
+        color: "blue-text-gradient",
       },
       {
-        name: 'tailwind css',
-        color: 'green-text-gradient',
+        name: "tailwind css",
+        color: "green-text-gradient",
       },
       {
-        name: 'API',
-        color: 'pink-text-gradient',
+        name: "API",
+        color: "pink-text-gradient",
       },
     ],
     image: lyriks,
-    repo: 'https://github.com/SimonGideon/lyrics',
-    demo: 'https://sg-lyriks.netlify.app/',
-    stack: [
-      reactjs,
-      redux,
-      css,
-      tailwind,
-    ]
+    repo: "https://github.com/SimonGideon/lyrics",
+    demo: "https://sg-lyriks.netlify.app/",
+    stack: [reactjs, redux, css, tailwind],
   },
 ];
 
